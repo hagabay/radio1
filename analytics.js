34bd5169b7907c712cb03420d3e98ea8282825e5
moti <hagabay@gmail.com>
2015-08-27 01:35:04 +0300

    2

--- analytics.js
+++ analytics.js
@@ -5,7 +5,6 @@ var myAnalyticsAccount = "UA-66840604-1";
 function init() {
 	//StatusBar.overlaysWebView(true);
     alert("ready");
-    window.analytics.startTrackerWithId("UA-66840604-1")	;
-    window.analytics.trackView('Screen Title');
+    window.analytics.startTrackerWithId('UA-66840604-1')
 }
 
