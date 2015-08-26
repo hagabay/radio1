4146ed0f9c83275f4daeca73934aa85fdf58d3da
moti <hagabay@gmail.com>
2015-08-27 01:11:42 +0300

    1

--- analytics.js
+++ analytics.js
@@ -1,15 +1,10 @@
 document.addEventListener("deviceready", init, false);
 var myAnalyticsAccount = "UA-66840604-1";
 
-var testDiv;
-
-function errorHandler(e) {
-  //Lame - do nothing	
-	alert(e.toString());
-}
 
 function init() {
-	StatusBar.overlaysWebView(true);
-window.analytics.startTrackerWithId(myAnalyticsAccount)	
+	//StatusBar.overlaysWebView(true);
+    alert("ready");
+    window.analytics.startTrackerWithId("UA-66840604-1")	
 }
 
