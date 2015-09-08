7654e1ef9750f6cba677941536f3e5656851c634
moti <hagabay@gmail.com>
2015-09-08 09:45:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
+ 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -48,6 +48,6 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
-};
-
-app.initialize();
+};*/
+alert();
+//app.initialize();
